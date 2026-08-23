43a8aae3fe86d22111086e6be9e44c5b3749536e
sharan_personal <shoronshark@gmail.com>
2026-08-23 18:06:18 +0600

    fix(eco-config): memory usage updated according to server requirement

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,15 +3,21 @@ module.exports = {
     {
       name: "personal-finance-app",
       cwd: __dirname,
+
       script: "node_modules/next/dist/bin/next",
       args: "start -p 4030",
+
       instances: 1,
-      exec_mode: "cluster",
+      exec_mode: "fork",
+
       autorestart: true,
+      watch: false,
+
       min_uptime: "30s",
       max_restarts: 10,
-      max_memory_restart: "800M",
-      watch: false,
+
+      max_memory_restart: "450M",
+
       time: true,
 
       env: {
